a0a6857910c660e2cb83e2b7c391b5710a290525
Jesus Zapata <jesus@vauxoo.com>
2017-04-12 11:49:53 -0400

    Update

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -67,5 +67,6 @@ class Course(models.Model):
         default['test_23'] = _('Test 23')
         default['test_24'] = _('Test 24')
         default['test_25'] = _('Test 25')
+        default['test_26'] = _('Test 26')
         return super(Course, self).copy(default)
 
