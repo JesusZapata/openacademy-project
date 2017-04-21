050c6da8c4bb0d83b79a44f23af52d864936ac00
Jesus Zapata <jesus@vauxoo.com>
2017-04-21 14:47:26 -0400

    [REF] i18n: Conflict on the daily cron (#24)
    * Test
    
    * [REF] i18n: Updating translation terms from weblate [ci skip]
    
    * [REF] i18n: Conflict on the daily cron
    
    On branch 8.0-vx
    Your branch is ahead of 'origin/8.0-vx' by 2 commits.
      (use "git push" to publish your local commits)
    Unmerged paths:
      (use "git reset HEAD <file>..." to unstage)
      (use "git add <file>..." to mark resolution)
    
    	both modified:   openacademy/i18n/es_VE.po
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    * Update

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -47,8 +47,6 @@ class Course(models.Model):
         default['test_3'] = _('Test 3')
         default['test_4'] = _('Test 4')
         default['test_5'] = _('Test 5')
-        default['test_6'] = _('Test 6')
-        default['test_7'] = _('Test 7')
         default['test_8'] = _('Test 8')
         default['test_9'] = _('Test 9')
         default['test_10'] = _('Test 10')
